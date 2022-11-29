01c59bfa794aeea62a0af400c3164d394fc67fdc
GreatDistances <117937385+GreatDistances@users.noreply.github.com>
2022-11-28 18:47:42 -0600

    updated

--- script.js
+++ script.js
@@ -6,7 +6,31 @@ window.addEventListener("load", function() {
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
         response.json().then(function(json) {
             console.log(json);
-            for (let i=0; i < json.length; i++) {
+            
+            // NUMBER OF ASTRONAUTS
+            for (let i=0; i <= json.length; i++) {
+                let astroCount = 0;
+                astroCount = i;
+                astro.innerHTML = `Astronauts: ${astroCount} total`
+            }
+
+            // DISPLAY ALL ASTRONAUT INFORMATION
+            for (key in json) {
+                    console.log(json[key]);
+                    container.innerHTML = `
+                        <div>
+                            <h3>${json[key].firstName} ${json[key].lastName}</h3>
+                            <h4><ul>
+                                <li>Hours In Space: ${json[key].hoursInSpace}</li>
+                                <li>Active: ${json[key].active}</li>
+                                <li>Skills: ${json[key].skills.join(', ')}</li>
+                            </ul></h4>
+                            <img class="avatar" src=${json[key].picture}>
+                        </div>
+                    `;
+                };
+            
+            /*for (let i=0; i < json.length; i++) {
                 for (let j=0; j < json[i].skills.length; j++) {
                     container.innerHTML = `
                         <div>
@@ -20,14 +44,16 @@ window.addEventListener("load", function() {
                         </div>
                     `;
                 }
-            }
+            
+            };
+            */
         });
     });
 });
 
 /* problems:
 
-1.  i loop and j loop both seem to retrieve last piece of data in objects.  Why?
+1.  both ai loop and j loop both seem to retrieve last piece of data in objects.  Why?
 2.  i loop and j loop do not print more than the final item in JSON object.  Why?
 3.  Picture not aligning correctly to text.  How to fix?
 4.  border not appearing around astronaut, unclear why since it was in CSS
