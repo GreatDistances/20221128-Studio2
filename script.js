eb1bc06f0096ca389f9cba187da59f62dbc6815c
GreatDistances <117937385+GreatDistances@users.noreply.github.com>
2022-11-28 20:27:37 -0600

    submitted

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
 // TODO: add code here
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
     const container = document.getElementById("container");
     console.log("page is loaded");
-    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
-        response.json().then(function(json) {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
+        response.json().then(function (json) {
             console.log(json);
-            
+
             // NUMBER OF ASTRONAUTS
-            for (let i=0; i <= json.length; i++) {
-                let astroCount = 0;
-                astroCount = i;
-                astro.innerHTML = `Astronauts: ${astroCount} total`
-            }
+            astro.innerHTML = `Astronauts: ${json.length} total`
+
+            // SORT ARRAY OF OBJECTS - NOT FUNCTIONAL**
+            //json.sort(function(a, b) {
+            //    return (a.hoursInSpace < b.hourseInSpace ? 1 : -1);
+            //});
 
             // DISPLAY ALL ASTRONAUT INFORMATION
             for (key in json) {
-                    console.log(json[key]);
-                    container.innerHTML = `
-                        <div>
-                            <h3>${json[key].firstName} ${json[key].lastName}</h3>
-                            <h4><ul>
-                                <li>Hours In Space: ${json[key].hoursInSpace}</li>
-                                <li>Active: ${json[key].active}</li>
-                                <li>Skills: ${json[key].skills.join(', ')}</li>
-                            </ul></h4>
-                            <img class="avatar" src=${json[key].picture}>
-                        </div>
-                    `;
-                };
-            
+                console.log(json[key]);
+                let activeClass = 'class="green"'
+                container.innerHTML += `
+                    <div class="astronaut">
+                        <h3>${json[key].firstName} ${json[key].lastName}</h3>
+                        <ul>
+                            <li>Hours In Space: ${json[key].hoursInSpace}</li>
+                            <li>Active: ${json[key].active}</li>
+                            <li>Skills: ${json[key].skills.join(', ')}</li>
+                        </ul>
+                        <img class="avatar" src=${json[key].picture}>
+                    </div>
+                `;
+            };
+
+            // ACTIVE TRUE = GREEN
+            //if (activeStatus.innerHTML = true) {
+            //    document.getElementById("activeStatus").style.color = "green";
+            //}
+
             /*for (let i=0; i < json.length; i++) {
                 for (let j=0; j < json[i].skills.length; j++) {
                     container.innerHTML = `
@@ -44,9 +51,9 @@ window.addEventListener("load", function() {
                         </div>
                     `;
                 }
-            
             };
             */
+            
         });
     });
 });
